281decd8a748e6f83d58e857dcf20e7ab4751ac2
Swathisri <90183448+Swathisri-02@users.noreply.github.com>
2025-05-01 20:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,5 @@ def generate_chart():
     buf.seek(0)
     base64_image = base64.b64encode(buf.read()).decode("utf-8")
 
-    return jsonify({
-    "image_url": f"data:image/png;base64,{base64_image}"
-    })
+    return base64_image
     
